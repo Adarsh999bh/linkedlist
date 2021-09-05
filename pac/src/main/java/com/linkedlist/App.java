21db42e9d003bd1cf0bc32eeb88beb50255148fa
Adarsh999bh <adarshbhandary48@gmail.com>
2021-09-06 03:50:29 +0530

    main method modified (insertion statements added)

--- pac/src/main/java/com/linkedlist/App.java
+++ pac/src/main/java/com/linkedlist/App.java
@@ -6,8 +6,26 @@ package com.linkedlist;
  */
 public class App 
 {
+    /**
+	 * 
+	 * This method is the main method Object object of LinkedList is created to
+	 * insert integers
+	 * 
+	 */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "welcme to linked list program" );
+        Linkedlist<Integer> object = new Linkedlist<>();
+		object.insert(56);
+		object.insert(30);
+		object.insert(40);
+		object.insert(70);
+		object.show();
+		if(object.search(40)) {
+			object.deleteAt(object.key);
+			object.show();
+		}
+		object.size();
+
     }
 }
